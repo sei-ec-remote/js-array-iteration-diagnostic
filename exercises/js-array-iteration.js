5dba7ff1a28cc6c85f1f9dc028f0c24eeeef5017
Alys <alice.g.cooper@gmail.com>
2022-06-28 09:27:09 -0400

    Completed exercises

--- exercises/js-array-iteration.js
+++ exercises/js-array-iteration.js
@@ -20,7 +20,8 @@ Question 1
 
 Given the array above, return the element in the `holidays` array that contains the value of `may` in the `month` key.
 */
-const mayHoliday = holidays.find(() => {
+const mayHoliday = holidays.find((holiday) => {
+  return holiday.month === 'may';
 })
 
 const words = ['cat', 'bath', 'orange', 'tap', 'bay', 'ha', 'extravagant']
@@ -30,8 +31,8 @@ Question 2
 
 Given the array above, return all elements in the `words` array that have less than 4 characters. 
 */
-const wordsWithLessThanFourLetters = words.filter(() => {
-
+const wordsWithLessThanFourLetters = words.filter((word) => {
+  return word.length < 4;
 })
 
 /*
@@ -41,8 +42,8 @@ const words = ['cat', 'bath', 'orange', 'tap', 'bay', 'ha', 'extravagant']
 
 Given the array above, return true if all of the strings in the `words` array contain the letter `a`. 
 */
-const doesEveryWordContainA = words.every(() => {
-
+const doesEveryWordContainA = words.every((word) => {
+  return word.includes('a') ? true : false;
 })
 
 /*
@@ -52,8 +53,8 @@ const words = ['cat', 'bath', 'orange', 'tap', 'bay', 'ha', 'extravagant']
 
 Given the array above, return true if any of the strings in the `words` array contain the letter `x`. 
 */
-const doesAnyWordContainX = words.some(() => {
-
+const doesAnyWordContainX = words.some((word) => {
+  return word.includes('x') ? true : false;
 })
 
 const developersArray = [
@@ -88,9 +89,26 @@ The finished object should look like:
   ]
 }
 */
-const developersObject = developersArray.reduce(() => {
+function groupBy(objArr, property) {
+  return objArr.reduce(function (acc, obj) {
+    let key = obj[property];
+    if (!acc[key]) {
+      acc[key] = []
+    }
+    acc[key].push(obj)
+    return acc
+  }, {})
+}
 
-}, {javascript: [], ruby: []})
+let developersObject = groupBy(developersArray, 'language')
+
+// const developersObject = developersArray.reduce(() => {
+//   if (developer['language'] === 'javascript') {
+//     javascript.push(developer);
+//   } else if (developer['language'] === 'ruby') {
+//     ruby.push(developer);
+//   }
+// }, {javascript: [], ruby: []})
 
 /* !!! DO NOT MODIFY ANYTHING BELOW HERE !!! */
 module.exports = {
